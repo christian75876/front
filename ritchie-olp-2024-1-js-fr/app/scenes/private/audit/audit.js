5a45260ac7e9e1f4ee0ad8efa7cf9f36c401faa8
Christian Pabon <christian75876@gmail.com>
2024-05-16 12:37:45 -0500

    Add event filter in audit

--- ritchie-olp-2024-1-js-fr/app/scenes/private/audit/audit.js
+++ ritchie-olp-2024-1-js-fr/app/scenes/private/audit/audit.js
@@ -33,51 +33,51 @@ export function AuditScene() {
    `;
 
     const logic = () => {
-      return;
+      const menus = document.querySelectorAll('a');
+   const element = document.getElementById('filter');
+   let aux2 = false;
+  
+   menus.forEach(menu => {
+     menu.addEventListener('click', () => {
+       if (aux2) return;
+       element.style.display = 'block';
+       const form = `
+       <form id="filterForm" class="filter-form">
+         <label class="filter-label" for="filterType">Filter By:</label>
+         <select class="filter-input" id="filterType" name="filterType">
+           <option value="userId">User ID</option>
+           <option value="clan">Clan</option>
+           <option value="role">Role</option>
+         </select>
+
+         <label class="filter-label" for="filterValue">Filter Value:</label>
+         <input class="filter-input" type="text" id="filterValue" name="filterValue">
+
+         <label class="filter-label" for="sortOrder">Sort Order:</label>
+         <select class="filter-input" id="sortOrder" name="sortOrder">
+           <option value="asc">Ascending</option>
+           <option value="desc">Descending</option>
+         </select>
+
+         <button type="submit" class="filter-button">Filter</button>
+         <button id="closeFormButton" class="filter-button">Close</button>
+       </form>
+     `;
+       element.innerHTML += form;
+       aux2 = true;
+  
+       const closeButton = document.getElementById('closeFormButton');
+       closeButton.addEventListener('click', () => {
+         aux2 = false;
+         element.style.display = 'none';
+       });
+     });
+   });
     };
   
 
 
-  // const menus = document.querySelectorAll('a');
-  // const element = document.getElementById('filter');
-  // let aux2 = false;
-  
-  // menus.forEach(menu => {
-  //   menu.addEventListener('click', () => {
-  //     if (aux2) return;
-  //     element.style.display = 'block';
-  //     const form = `
-  //     <form id="filterForm" class="filter-form">
-  //       <label class="filter-label" for="filterType">Filter By:</label>
-  //       <select class="filter-input" id="filterType" name="filterType">
-  //         <option value="userId">User ID</option>
-  //         <option value="clan">Clan</option>
-  //         <option value="role">Role</option>
-  //       </select>
-
-  //       <label class="filter-label" for="filterValue">Filter Value:</label>
-  //       <input class="filter-input" type="text" id="filterValue" name="filterValue">
-
-  //       <label class="filter-label" for="sortOrder">Sort Order:</label>
-  //       <select class="filter-input" id="sortOrder" name="sortOrder">
-  //         <option value="asc">Ascending</option>
-  //         <option value="desc">Descending</option>
-  //       </select>
-
-  //       <button type="submit" class="filter-button">Filter</button>
-  //       <button id="closeFormButton" class="filter-button">Close</button>
-  //     </form>
-  //   `;
-  //     element.innerHTML += form;
-  //     aux2 = true;
-  
-  //     const closeButton = document.getElementById('closeFormButton');
-  //     closeButton.addEventListener('click', () => {
-  //       aux2 = false;
-  //       element.style.display = 'none';
-  //     });
-  //   });
-  // });
+   
 
   return {
     pageContent,
