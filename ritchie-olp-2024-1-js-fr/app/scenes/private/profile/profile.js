95dbcf43db0795045b6a4db919849d3f69c0c104
Christian Pabon <christian75876@gmail.com>
2024-05-22 10:23:21 -0500

    Add user in profile with endpoint dinamic

--- ritchie-olp-2024-1-js-fr/app/scenes/private/profile/profile.js
+++ ritchie-olp-2024-1-js-fr/app/scenes/private/profile/profile.js
@@ -5,22 +5,33 @@ import roket from '../../../assets/img/rocket.png'
 export function Profile() {
 
   const pageContent = `
-    <div class="${styles.container}">
+    <div id="user-info" class="${styles.container}">
+      
+    </div>
+    `;
+
+  const logic = async () => {
+    const token = localStorage.token;
+    console.log('token');
+
+    let payload = token.split('.')[1];
+    let decodedPayload = JSON.parse(atob(payload));
+    let id = decodedPayload.id;
+
+    const resp = await fetch(`http://localhost:4000/api/users/${id}/`);
+    const user = await resp.json();
+    const userInfo = document.getElementById('user-info');
+    userInfo.innerHTML = `
       <table class="${styles['user-details-table']}">
-        <tr><th>ID</th><td></td></tr>
-        <tr><th>Name</th><td></td></tr>
-        <tr><th>User</th><td></td></tr>
-        <tr><th>Password</th><td></td></tr>
-        <tr><th>Clan</th><td></td></tr>
-        <tr><th>Phone</th><td></td></tr>
+        <tr><th>Id</th><td>${user.id}</td></tr>
+        <tr><th>User name</th><td>${user.username}</td></tr>
+        <tr><th>Email</th><td>${user.email}</td></tr>
       </table>
       <div class="${styles['container-img']}">
         <img src="${roket}" class="${styles.img}" alt="rocket" style="background-image: url(${roket});">
       </div>
-    </div>
-    `;
-
-  const logic = () => {
+    `
+    
 
   }
 
