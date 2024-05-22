47cc3858abf426f04de1cb26ee4077b93edefaae
rafez963 <rafaelmeza963san@gmail.com>
2024-05-21 21:15:18 -0500

    setting in process

--- ritchie-olp-2024-1-js-fr/app/scenes/private/settings/settings.js
+++ ritchie-olp-2024-1-js-fr/app/scenes/private/settings/settings.js
@@ -2,10 +2,35 @@ import { DashboardLayout } from '../../../components/layout/private/dashboard/da
 import styles from './settings.css';
 
 export function SettingsScene() {
+  const email = localStorage.getItem('email')
+  
+  const password = localStorage.getItem('password') 
+
 
   const pageContent = `
-      <h2>Settings</h2>
-      <p>Welcome to the settings view.</p>
+      <h2 class='${styles['title-h2']}'>Seccion Usuarios</h2>
+      <div class='${styles['form-container']}'>
+        <form action="#" method="post">
+            <h2>Actualización de Contraseña</h2>
+            <div class='${styles['form-group']}'>
+                <label for="username"> Email</label>
+                <input type="text" id="username" name="username" value=${email} disabled>
+            </div>
+            <div class='${styles['form-group']}'>
+                <label for="current-password">Contraseña Actual</label>
+                <input type="password" id="current-password" name="current-password" required>
+            </div>
+            <div class='${styles['form-group']}'>
+                <label for="new-password">Nueva Contraseña</label>
+                <input type="password" id="new-password" name="new-password" required>
+            </div>
+            <div class='${styles['form-group']}'>
+                <label for="confirm-password">Verificación de Nueva Contraseña</label>
+                <input type="password" id="confirm-password" name="confirm-password" required>
+            </div>
+            <button type="submit">Actualizar</button>
+        </form>
+  </div>
     `;
 
   const logic = () => {
